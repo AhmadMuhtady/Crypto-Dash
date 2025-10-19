ae5e5a9b1a728528c37bd02ae7a88c81c64062eb
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-10-19 22:14:16 +0300

    Add FilterInput component and integrate filtering functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Coin from './components/Coin';
 import LimitSelect from './components/LimitSelect';
+import FilterInput from './components/filterInput';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -9,6 +10,8 @@ const App = () => {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
 	const [limit, setLimit] = useState(10);
+	const [filter, setFilter] = useState('');
+	const [sortBy, setSortBy] = useState('market_cap_desc');
 
 	useEffect(() => {
 		const fetchCoins = async () => {
@@ -30,19 +33,32 @@ const App = () => {
 		fetchCoins();
 	}, [limit]);
 
+	const filteredCoins = coins.filter((coin) => {
+		return (
+			coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+			coin.symbol.toLowerCase().includes(filter.toLowerCase())
+		);
+	});
+
 	return (
 		<div>
 			<h1>🚀 Crypto Dash</h1>
 
 			{loading && <p>Loading...</p>}
 			{error && <div className="error">{error}</div>}
-			<LimitSelect limit={limit} onChangeLimit={setLimit} />
+
+			<div className="top-controls">
+				<FilterInput filter={filter} onFilterChange={setFilter} />
+				<LimitSelect limit={limit} onChangeLimit={setLimit} />
+			</div>
 
 			{!loading && !error && (
 				<main className="grid">
-					{coins.map((coin) => (
-						<Coin key={coin.id} coin={coin} />
-					))}
+					{filteredCoins.length > 0 ? (
+						filteredCoins.map((coin) => <Coin key={coin.id} coin={coin} />)
+					) : (
+						<p>No matching coins</p>
+					)}
 				</main>
 			)}
 		</div>
