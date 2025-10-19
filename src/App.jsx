cd77f74a3d4a39d0582e415fb3a624ef5cef542e
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-10-19 21:10:02 +0300

    Add LimitSelect component for dynamic coin limit selection and integrate it into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-
 import Coin from './components/Coin';
+import LimitSelect from './components/LimitSelect';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -8,16 +8,17 @@ const App = () => {
 	const [coins, setCoins] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [limit, setLimit] = useState(10);
 
 	useEffect(() => {
 		const fetchCoins = async () => {
+			setLoading(true);
 			try {
 				const res = await fetch(
-					`${API_URL}'&order=market_cap_desc&per_page=10&page=1&sparkLine=false'`
+					`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkLine=false`
 				);
-				if (!res.ok) throw Error('Failed to fetch Data');
+				if (!res.ok) throw new Error('Failed to fetch data');
 				const data = await res.json();
-				console.log(data);
 				setCoins(data);
 			} catch (err) {
 				setError(err.message);
@@ -27,13 +28,15 @@ const App = () => {
 		};
 
 		fetchCoins();
-	}, []);
+	}, [limit]);
 
 	return (
 		<div>
 			<h1>🚀 Crypto Dash</h1>
+
 			{loading && <p>Loading...</p>}
 			{error && <div className="error">{error}</div>}
+			<LimitSelect limit={limit} onChangeLimit={setLimit} />
 
 			{!loading && !error && (
 				<main className="grid">
