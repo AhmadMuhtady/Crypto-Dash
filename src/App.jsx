4da4bdeffbcc4836f272a4b367dd5a7463916b9d
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-10-20 20:23:43 +0300

    Refactor App component to use HomePage for rendering and implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
-import Coin from './components/Coin';
-import LimitSelect from './components/LimitSelect';
-import FilterInput from './components/filterInput';
-import SortSelector from './components/SortSelector';
+import { Route, Routes } from 'react-router';
+import HomePage from './Pages/HomePage';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -34,54 +32,25 @@ const App = () => {
 		fetchCoins();
 	}, [limit]);
 
-	const filteredCoins = coins
-		.filter((coin) => {
-			return (
-				coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-				coin.symbol.toLowerCase().includes(filter.toLowerCase())
-			);
-		})
-		.slice()
-		.sort((a, b) => {
-			switch (sortBy) {
-				case 'market_cap_desc':
-					return b.market_cap - a.market_cap;
-				case 'market_cap_asc':
-					return a.market_cap - b.market_cap;
-				case 'price_desc':
-					return b.current_price - a.current_price;
-				case 'price_asc':
-					return a.current_price - b.current_price;
-				case 'change_desc':
-					return b.price_change_percentage_24h - a.price_change_percentage_24h;
-				case 'change_asc':
-					return a.price_change_percentage_24h - b.price_change_percentage_24h;
-			}
-		});
-
 	return (
-		<div>
-			<h1>🚀 Crypto Dash</h1>
-
-			{loading && <p>Loading...</p>}
-			{error && <div className="error">{error}</div>}
-
-			<div className="top-controls">
-				<FilterInput filter={filter} onFilterChange={setFilter} />
-				<LimitSelect limit={limit} onChangeLimit={setLimit} />
-				<SortSelector sortBy={sortBy} onSortChange={setSortBy} />
-			</div>
-
-			{!loading && !error && (
-				<main className="grid">
-					{filteredCoins.length > 0 ? (
-						filteredCoins.map((coin) => <Coin key={coin.id} coin={coin} />)
-					) : (
-						<p>No matching coins</p>
-					)}
-				</main>
-			)}
-		</div>
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<HomePage
+						coins={coins}
+						filter={filter}
+						setFilter={setFilter}
+						limit={limit}
+						setLimit={setLimit}
+						sortBy={sortBy}
+						setSortBy={setSortBy}
+						loading={loading}
+						error={error}
+					/>
+				}
+			/>
+		</Routes>
 	);
 };
 
---
+++ src/Pages/homePage.jsx
@@ -0,0 +1,67 @@
+import Coin from '../components/Coin';
+import LimitSelect from '../components/LimitSelect';
+import FilterInput from '../components/filterInput';
+import SortSelector from '../components/SortSelector';
+
+const HomePage = ({
+	coins,
+	filter,
+	setFilter,
+	limit,
+	setLimit,
+	sortBy,
+	setSortBy,
+	loading,
+	error,
+}) => {
+	const filteredCoins = coins
+		.filter((coin) => {
+			return (
+				coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+				coin.symbol.toLowerCase().includes(filter.toLowerCase())
+			);
+		})
+		.slice()
+		.sort((a, b) => {
+			switch (sortBy) {
+				case 'market_cap_desc':
+					return b.market_cap - a.market_cap;
+				case 'market_cap_asc':
+					return a.market_cap - b.market_cap;
+				case 'price_desc':
+					return b.current_price - a.current_price;
+				case 'price_asc':
+					return a.current_price - b.current_price;
+				case 'change_desc':
+					return b.price_change_percentage_24h - a.price_change_percentage_24h;
+				case 'change_asc':
+					return a.price_change_percentage_24h - b.price_change_percentage_24h;
+			}
+		});
+	return (
+		<div>
+			<h1>🚀 Crypto Dash</h1>
+
+			{loading && <p>Loading...</p>}
+			{error && <div className="error">{error}</div>}
+
+			<div className="top-controls">
+				<FilterInput filter={filter} onFilterChange={setFilter} />
+				<LimitSelect limit={limit} onChangeLimit={setLimit} />
+				<SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+			</div>
+
+			{!loading && !error && (
+				<main className="grid">
+					{filteredCoins.length > 0 ? (
+						filteredCoins.map((coin) => <Coin key={coin.id} coin={coin} />)
+					) : (
+						<p>No matching coins</p>
+					)}
+				</main>
+			)}
+		</div>
+	);
+};
+
+export default HomePage;
